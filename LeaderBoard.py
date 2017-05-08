b1792b3d7e6c63f44fd71ad827f8a86220b827c4
MitchellKYoung <s3634235@student.rmit.edu.au>
2017-05-08 12:17:14 +1000

    Update LeaderBoard.py

--- LeaderBoard.py
+++ LeaderBoard.py
@@ -1,4 +1,14 @@
-#Leaderboard object
-#def leaderboard():
-    #leader list
-        #name+score
+def leaderboardpage():
+    leaderboardmenu = True
+    while leaderboardmenu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 75)
+        TextSurf, TextRect = text_objects("LEADERBOARD", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 3))
+        gameDisplay.blit(TextSurf, TextRect)
+        button("MAIN MENU", 350, 530, 160, 40, red, bright_red, 'mainMenu')
+        pygame.display.update()
+        clock.tick(60)
